5951636df017ca04727f07ce4eaaffdde0ef729c
Jacob Ebey <jacob.ebey@live.com>
2025-04-17 19:15:22 -0700

    add support for actions

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,15 +1,20 @@
 import { Route } from "./+types/home";
 
+import { log } from "./home.actions";
+
 export function loader({}: Route.LoaderArgs) {
   return "hello, world";
 }
 
-export default function Home({ loaderData }: Route.ComponentProps) {
+export function ServerComponent({ loaderData }: Route.ComponentProps) {
   return (
     <main>
       <h1>Home</h1>
       <p>This is the home page.</p>
       <p>loaderData: {loaderData}</p>
+      <form action={log}>
+        <button type="submit">Log on server</button>
+      </form>
     </main>
   );
 }
