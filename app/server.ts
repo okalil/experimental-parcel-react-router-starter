e18162b82fb19cbb2a79b1fb9e33678f394151b1
Matt Brophy <matt@brophy.org>
2025-05-14 12:16:59 -0400

    Update to latest parcel plugin to handle 404 hydration errors

--- app/server.ts
+++ app/server.ts
@@ -4,6 +4,12 @@ import reactRouter from "virtual:react-router/express";
 
 const app = express();
 app.use(express.static("public"));
+
+app.get("/.well-known/appspecific/com.chrome.devtools.json", (req, res) => {
+  res.status(404);
+  res.send("Not found");
+});
+
 app.use(reactRouter);
 
 app.listen(3000);
